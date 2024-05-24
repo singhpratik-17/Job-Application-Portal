cb60bc320dd3a80d279cd76473d38db9e641b096
singhpratik-17 <singhpratik455@gmail.com>
2024-05-25 01:50:12 +0530

    first commit

---
+++ Frontend/src/components/Layout/Footer.jsx
@@ -0,0 +1,24 @@
+import React, { useContext } from "react";
+import { Context } from "../../main";
+import { Link } from "react-router-dom";
+import { FaFacebookF, FaYoutube, FaLinkedin } from "react-icons/fa";
+import { RiInstagramFill } from "react-icons/ri";
+
+const Footer = () => {
+  const { isAuthorized } = useContext(Context);
+  return (
+    <footer className={isAuthorized ? "footerShow" : "footerHide"}>
+      <div>&copy; All Rights Reserved By JobZee.</div>
+      <div>
+        <Link to={"https://www.youtube.com"} target="_blank">
+          <FaYoutube />
+        </Link>
+        <Link to={"https://https://www.linkedin.com"} target="_blank">
+          <FaLinkedin />
+        </Link>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
