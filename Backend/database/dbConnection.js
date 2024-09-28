2a58d3cc41d46629fefb571305b215425c0336a6
Pratik Singh <157296309+singhpratik-17@users.noreply.github.com>
2024-09-29 01:11:47 +0530

    Update dbConnection.js

--- Backend/database/dbConnection.js
+++ Backend/database/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbConnection = () => {
   mongoose
-    .connect(process.env.MONGO_URI, {
+    .connect(process.env.MONGO_URL, {
       dbName: "JOB_APPLICATION_PORTAL",
     })
     .then(() => {
